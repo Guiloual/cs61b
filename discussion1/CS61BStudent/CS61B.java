4987e9da35e56dddf5f32fb90e5b0531c47172a3
Jian Chern <cj1670832374@163.com>
2025-02-26 12:28:53 +0800

    Finished CS61B.java

--- discussion1/CS61BStudent/CS61B.java
+++ discussion1/CS61BStudent/CS61B.java
@@ -2,46 +2,52 @@ import java.lang.reflect.Array;
 
 public class CS61B {
 
-    public String university = "UC Berkeley";
+    public static String university = "UC Berkeley";
     public String semester;
-    public CS61BStudent students;
+    public CS61BStudent[] students;
     public int capacity;
-    private CS61BStudent[] sigups;
-    public CS61BStudent[] enrollable;
-    public int wathNumber = 0;
+    //private CS61BStudent[] sigups;
+    //public CS61BStudent[] enrollable;
+    //public int wathNumber = 0;
 
 
     public CS61B(CS61BStudent[] sigups, String semester,int capacity){
         this.capacity = capacity;
         this.semester = semester;
-        this.sigups = sigups;
-        if (sigups.length <= capacity) {
-            System.arraycopy(enrollable,0,sigups,0,sigups.length);
-        }else {
-            System.arraycopy(enrollable, 0, sigups, 0, capacity);
+        this.students = new CS61BStudent[capacity];
+        for (int i=0; i<capacity; i++){
+            students[i] = sigups[i];
         }
+        //if (sigups.length <= capacity) {
+        //    System.arraycopy(enrollable,0,sigups,0,sigups.length);
+        //}else {
+        //    System.arraycopy(enrollable, 0, sigups, 0, capacity);
+        //}
     }
 
 
 
-    public void newUniversity(String newuniversity) {
+    public static void newUniversity(String newuniversity) {
         university = newuniversity;
     }
 
-    public int makeStudentsWatchLecture(CS61BStudent stu) {
-        if (stu.watchLecture()) {
-            wathNumber += 1;
+    public int makeStudentsWatchLecture() {
+        int wathNumber=0;
+        for (CS61BStudent student : students) {
+            if (student.watchLecture()) {
+                wathNumber += 1;
+            }
         }
         return wathNumber;
     }
 
-    public void expand (int ext) {
-        capacity += ext;
-        if (sigups.length <= capacity) {
-            System.arraycopy(enrollable,0,sigups,0,sigups.length);
-        }else {
-            System.arraycopy(enrollable, 0, sigups, 0, capacity);
-        }
+    public void expand (int newCapacity) {
+        capacity = newCapacity;
+        //if (sigups.length <= capacity) {
+        //    System.arraycopy(enrollable,0,sigups,0,sigups.length);
+        //}else {
+        //    System.arraycopy(enrollable, 0, sigups, 0, capacity);
+        //}
         //System.arraycopy(enrollable,0,sigups,0 , capacity);
     }
 
