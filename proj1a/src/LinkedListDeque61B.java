30956f5a983b3e9d9a50145d44de4b34094c8e58
Jian Chern <cj1670832374@163.com>
2025-03-12 23:12:24 +0800

    write addFirst, addLast and toList but still have some bugs

--- proj1a/src/LinkedListDeque61B.java
+++ proj1a/src/LinkedListDeque61B.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class LinkedListDeque61B <T> implements Deque61B<T>{
@@ -19,22 +20,49 @@ public class LinkedListDeque61B <T> implements Deque61B<T>{
 
     public LinkedListDeque61B() {
         sentinel = new Node(sentinel, null, sentinel);
+        //sentinel = new Node(sentinel, null, sentinel);
     }
 
 
     @Override
     public void addFirst(T x) {
-
+        //sentinel.rest = new Node(sentinel, x, sentinel.rest);
+        if (sentinel.rest == null) {
+            sentinel.rest = new Node(sentinel, x, sentinel.rest);
+            sentinel.rest.rest = sentinel;
+            sentinel.first = sentinel.rest;
+        } else {
+            sentinel.rest = new Node(sentinel, x, sentinel.rest);
+        }
+//        if (sentinel.rest == null) {
+//            sentinel.rest = new Node(sentinel, x, sentinel);
+//        } else {
+//            sentinel.rest = new Node(sentinel, x, sentinel.rest);
+//        }
     }
 
     @Override
     public void addLast(T x) {
-
+        //sentinel.first = new Node(sentinel.first, x, sentinel);
+        if (sentinel.first == null) {
+            sentinel.first = new Node(sentinel.first, x, sentinel);
+            sentinel.first.first = sentinel;
+            sentinel.rest = sentinel.first;
+        } else {
+            sentinel.first = new Node(sentinel.first, x, sentinel);
+        }
     }
 
     @Override
     public List<T> toList() {
-        return List.of();
+        List<T> returnList = new ArrayList<>();
+        Node p = sentinel.rest;
+        if (p == null) {return returnList;}
+        while (p != sentinel) {
+            returnList.add(p.item);
+            p = p.rest;
+        }
+        return returnList;
     }
 
     @Override
